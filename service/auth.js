afa09a395505363adda5e75c0886e50c0a32d5ca
Rishabh Gupta <dalbazarrishabhgupta@gmail.com>
2024-06-15 20:04:16 +0530

    Authentication using jwt

--- service/auth.js
+++ service/auth.js
@@ -1,9 +1,26 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Rishabh$$Gupta%%";
 
-function setUser(id, user) {
-	sessionIdToUserMap.set(id, user);
+function setUser(user) {
+	// sessionIdToUserMap.set(id, user);
+	const payload = {
+		_id: user._id,
+		email: user.email,
+	};
+
+	return jwt.sign(payload, secret);
 }
-function getUser(id) {
-	return sessionIdToUserMap.get(id);
+function getUser(token) {
+	// return sessionIdToUserMap.get(id);
+
+	if (!token) {
+		return null;
+	}
+	try {
+		return jwt.verify(token, secret);
+	} catch (error) {
+		return null;
+	}
 }
 module.exports = { setUser, getUser };

526269e68f65210076f7af2e48ec8ac84b89def8
Rishabh Gupta <dalbazarrishabhgupta@gmail.com>
2024-06-23 07:37:31 +0530

    env added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -5,13 +6,13 @@ const cookieParser = require("cookie-parser");
 // const { restrictToLoggedinUserOnly, checkAuth } = require("./middlewares/auth");
 const { checkForAuthentication, restrictTo } = require("./middlewares/auth");
 const connectToMongo = require("./db");
-const PORT = 8001;
+const PORT = process.env.PORT || 8003;
 const URL = require("./models/url");
 const urlRoute = require("./routes/url");
 const staticRoute = require("./routes/staticRouter");
 const userRoute = require("./routes/user");
 
-connectToMongo("mongodb://127.0.0.1:27017/short-url");
+connectToMongo(process.env.MONGO_URI ?? "mongodb://127.0.0.1:27017/short-url");
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
